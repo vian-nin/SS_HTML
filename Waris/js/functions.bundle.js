25ac06d2af32b2eec2a948c8b925477c1cb4a433
vian-nin <vian.nin00@gmail.com>
2024-10-15 15:28:12 +0700

    update page redem point

--- Waris/js/functions.bundle.js
+++ Waris/js/functions.bundle.js
@@ -8904,3 +8904,20 @@ jQuery(function() {
 
 	
 });
+
+// copy form
+
+function copyText() {
+	// Get the text field
+	var copyText = document.getElementById("myInput");
+  
+	// Select the text field
+	copyText.select();
+	copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+	// Copy the text inside the text field
+	navigator.clipboard.writeText(copyText.value);
+	
+	// Alert the copied text
+	alert("Copied the text: " + copyText.value);
+  }
